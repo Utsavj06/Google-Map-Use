7b49b94d272e3395fbc66660b1e79048a9efcd0b
Utsav <utsav96jaiswal@gmail.com>
2024-01-22 19:10:53 +0530

    second commit

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,5 +1,6 @@
 import { GoogleMap, Marker } from '@react-google-maps/api';
-import React from 'react'
+import React, { Fragment } from 'react'
+import locMarker from '../152964.svg'
 
 const Map = ({isLoaded}) => {
     const containerStyle = {
@@ -14,8 +15,31 @@ const Map = ({isLoaded}) => {
 
       const ballia = {
         lat: 25.3176,
-        long:  82.9739
+        lng:  82.9739
       }
+
+      const markers = [
+       { name: 'mgs',
+        location: {
+          lat: 25.2838,
+          lng: 83.1156 
+        }},
+       { name: 'vns',
+        location: {
+          lat: 25.3176,
+        lng:  82.9739
+        }},
+       { name: 'chandauli',
+        location: {
+          lat: 25.2605,
+          lng: 83.2645
+        }},
+       { name: 'kanpur',
+        location: {
+          lat: 25.3502,
+          lng: 83.2577
+        }},
+      ]
   return (
     <div>
         {isLoaded && <>
@@ -24,8 +48,13 @@ const Map = ({isLoaded}) => {
         center={center}
         zoom={10}
       >
-        <Marker position={center} />
-        <Marker position={ballia} />
+        {/*<Marker position={center} />
+        <Marker position={ballia} /> */}
+        {markers.map(location=> <Fragment key={location.name}>
+        <Marker position={location.location} options={{
+          icon: locMarker
+        }}/>
+        </Fragment>)}
       </GoogleMap>
       </>}
     </div>
